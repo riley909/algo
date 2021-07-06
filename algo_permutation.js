647ff1f449e3891e50905c3fd94c4ec7f3329724
riley909 <increaseblue666@gmail.com>
2021-07-07 00:30:49 +0900

    algo_permutation__in_progress

--- algo_permutation.js
+++ algo_permutation.js
@@ -1,13 +1,30 @@
 const input = ['A', 'B', 'C', 'D'];
 const answer = [];
 
-function permutaion(arr, n, k) {
+function permutaion(arr, n, r) {
+  console.log(n);
+  console.log(arr.length);
   if (n === arr.length - 1) {
-    const str = arr.slice(0, k).join('');
-    if (!answer.includes(str)) answer.push(str);
-    return;
+    const str = arr.slice(0, r).join('');
+    if (!answer.includes(str)) {
+      answer.push(str);
+      return;
+    }
   } else {
+    for (let i = n; i < arr.length; i++) {
+      let temp = arr[n];
+      console.log(temp);
+      arr[n] = arr[i];
+      arr[i] = temp;
+      console.log(arr[n], arr[i]);
+
+      permutaion(arr, n + 1, r);
+
+      arr[i] = arr[n];
+      arr[n] = temp;
+    }
+    return answer;
   }
 }
-
+console.log(answer);
 console.log(permutaion(input, 0, 2));
