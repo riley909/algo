1e84e3d5dea854e36b7d0eeba27e8407d9317b8d
riley909 <increaseblue666@gmail.com>
2021-07-09 00:54:36 +0900

    prog2-16__in_progress

---
+++ programmers/prog2/prog2-16.js
@@ -0,0 +1,19 @@
+// NOTE: 스킬트리
+// https://programmers.co.kr/learn/courses/30/lessons/49993
+// 선행스킬 순서에 따라서 스킬을 배워야 함
+// skill = 선행 스킬 순서 배열
+// skill_trees = 유저들이 만든 스킬트리 배열
+// 가능한 스킬트리의 개수 리턴
+
+// skill = 'CBD'
+// skill_trees = ['BACDE', 'CBADF', 'AECB', 'BDA']
+
+function solution(skill, skill_trees) {
+  var answer = -1;
+  return answer;
+}
+
+const skill = 'CBD';
+const skill_trees = ['BACDE', 'CBADF', 'AECB', 'BDA'];
+
+console.log(solution(skill, skill_trees));
